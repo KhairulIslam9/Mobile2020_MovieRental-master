7b97b4b0a4e51c1f7f72610b550878d490815017
Khairul Islam <kaiislam1997@gmail.com>
2020-11-22 20:19:49 +0100

    Actor

---
+++ MovieRental.API/Controllers/ActorController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieRental.DAL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRental.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActorController : ControllerBase
+    {
+        private ActorService _service;
+
+        public ActorController()
+        {
+            _service = new ActorService();
+
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                return Ok(_service.GetAll());
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("{Id:int}")]
+        public IActionResult GetById(int Id)
+        {
+            try
+            {
+                return Ok(_service.GetById(Id));
+            }catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("Title")]
+        public IActionResult getByFilm(string Title)
+        {
+            try
+            {
+                return Ok(_service.GetByFilm(Title));
+            }catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+        }
+
+        [HttpGet]
+        [Route("Initial")]
+        public IActionResult GetByInital(string Initial)
+        {
+            try
+            {
+                return Ok(_service.GetByIntial(Initial[0], Initial[1]));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+    }
+}
